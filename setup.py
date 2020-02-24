b42b0c30d3466af6e0dca1b66762336862bef72b
deeplook <gherman@darwin.in-berlin.de>
2020-02-24 11:29:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 setup(name='mcinstall',
       version='0.1.0',
       description='Install and provision a fresh Miniconda distribution from scratch.',
       author='Dinu Gherman',
       url='https://github.com/deeplook/mcinstall',
+      setup_requires=[] + pytest_runner,
+      tests_require=['pytest'],
       py_modules=['mcinstall'],
       entry_points={
         'console_scripts': ['mcinstall=mcinstall:main']})
