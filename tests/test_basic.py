a744678957b683a57f9287da478777b326b31c17
Dinu Gherman <gherman@darwin.in-berlin.de>
2020-02-25 01:11:39 +0100

    Use geopy as example

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,13 +20,13 @@ def test_uname_m():
     print(out.decode("utf-8"))
 
 
-def test_pip_sklearn():
+def test_pip_geopy():
     verbose = True
     with tempfile.TemporaryDirectory() as tempdir:
         mci = MinicondaInstaller(tempdir, verbose=verbose)
         mci.download()
         mci.install_miniconda()
-        mci.install_pip(dependencies=["scikit-learn"])
+        mci.install_pip(dependencies=["geopy"])
 
         # Find Python version used by the installed Miniconda, and add it to the
         # search path:
@@ -39,8 +39,8 @@ def test_pip_sklearn():
         sys.path.insert(0, f"{tempdir}/lib/{py_version}/site-packages")
 
         # Import the installed test package.
-        import sklearn
-        print(f"Sklearn location: {sklearn.__file__}")
-        print(f"Sklearn version: {sklearn.__version__}")
+        import geopy
+        print(f"Geopy location: {geopy.__file__}")
+        print(f"Geopy version: {geopy.__version__}")
 
     assert not os.path.exists(tempdir)
