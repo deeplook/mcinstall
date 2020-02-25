362ce1fdfbfb9cac8d814d3c5be3d203b80d700a
Dinu Gherman <gherman@darwin.in-berlin.de>
2020-02-25 01:06:03 +0100

    Determine Python used by Miniconda

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,17 +20,27 @@ def test_uname_m():
     print(out.decode("utf-8"))
 
 
-def test_pip_geopy():
+def test_pip_sklearn():
     verbose = True
     with tempfile.TemporaryDirectory() as tempdir:
         mci = MinicondaInstaller(tempdir, verbose=verbose)
         mci.download()
         mci.install_miniconda()
-        mci.install_pip(dependencies=["geopy"])
+        mci.install_pip(dependencies=["scikit-learn"])
 
-        sys.path.insert(0, f"{tempdir}/lib/python3.7/site-packages")
-        # import geopy
-        # print(f"Geopy location: {geopy.__file__}")
-        # print(f"Geopy version: {geopy.__version__}")
+        # Find Python version used by the installed Miniconda, and add it to the
+        # search path:
+        cmd = [
+            f"{tempdir}/bin/python", "-c",
+            '''import sys;v='.'.join(map(str,sys.version_info[:2]));print('python'+v)'''
+        ]
+        out = subprocess.check_output(cmd)
+        py_version = out.decode("utf-8").strip()
+        sys.path.insert(0, f"{tempdir}/lib/{py_version}/site-packages")
+
+        # Import the installed test package.
+        import sklearn
+        print(f"Sklearn location: {sklearn.__file__}")
+        print(f"Sklearn version: {sklearn.__version__}")
 
     assert not os.path.exists(tempdir)
