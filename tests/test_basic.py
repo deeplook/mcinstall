42f7fa818dcd20c02008d5f50c6d830562600029
Dinu Gherman <gherman@darwin.in-berlin.de>
2020-02-25 22:14:37 +0100

    Simplified install test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -21,39 +21,26 @@ def test_uname_m():
     print(out.decode("utf-8"))
 
 
-def test_pip():
-    verbose = True
+def test_install_dependencies():
     with tempfile.TemporaryDirectory() as tempdir:
-        mci = MinicondaInstaller(tempdir, verbose=verbose)
+        mci = MinicondaInstaller(tempdir, verbose=True)
         mci.download()
         mci.install_miniconda()
         mci.install_pip(dependencies=["geopy"])
+        mci.install_conda(channel="conda-forge", dependencies=["scikit-learn"])
 
-        # Run Miniconda's Python  and import the installed dependency.
-        cmd = [
-            f"{tempdir}/bin/python", "-c",
-            "import geopy; print('Imported geopy ' + geopy.__version__)"
-        ]
-        out = subprocess.check_output(cmd).decode("utf-8").strip()
-        print(out)
-
-    assert not os.path.exists(tempdir)
-
-
-def test_conda():
-    verbose = True
-    with tempfile.TemporaryDirectory() as tempdir:
-        mci = MinicondaInstaller(tempdir, verbose=verbose)
-        mci.download()
-        mci.install_miniconda()
-        mci.install_conda(channel="conda-forge", dependencies=["numpy"])
-
-        # Run Miniconda's Python  and import the installed dependency.
-        cmd = [
-            f"{tempdir}/bin/python", "-c",
-            "import numpy; print('Imported numpy '+numpy.__version__)"
-        ]
-        out = subprocess.check_output(cmd).decode("utf-8").strip()
-        print(out)
+        # Run Miniconda's Python and import the installed dependencies.
+        for pkg_name in ["geopy", "scikit-learn"]:
+            if pkg_name == "scikit-learn":
+                pkg_name = "sklearn"
+            cmd = [
+                f"{tempdir}/bin/python", "-c",
+                f"import {pkg_name}; "
+                f"print('{pkg_name} {pkg_name}.__version__ ok')"
+            ]
+            out = subprocess.check_output(cmd).decode("utf-8").strip()
+            print(out)
+            assert re.match(f"{pkg_name} .+ ok", out)
 
     assert not os.path.exists(tempdir)
+
