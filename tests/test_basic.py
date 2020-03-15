ee98bcf73f984591d471505c6257f4db82a78e84
omestry <omkar.mestry@here.com>
2020-03-15 15:03:43 +0530

    Changes to add index-url and extra-index-url command

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -46,3 +46,75 @@ def test_install_dependencies():
 
     print()
     assert not os.path.exists(tempdir)
+
+
+def test_install_dependencies_index_url():
+    with tempfile.TemporaryDirectory() as tempdir:
+        mci = MinicondaInstaller(tempdir, verbose=True)
+        mci.download()
+        mci.install_miniconda()
+        mci.update_miniconda_base()
+        mci.install_pip(dependencies=["pypi_pkg_test"], index_url="https://test.pypi.org/simple/")
+
+        # Run Miniconda's Python and import the installed dependencies.
+        for pkg_name in ["pypi_pkg_test"]:
+            cmd = [
+                "%s/bin/python" % tempdir,
+                "-c",
+                '''import %s; print("%s %%s ok" %% %s)''' % \
+                (pkg_name, pkg_name, pkg_name)
+            ]
+            out = subprocess.check_output(cmd).decode("utf-8").strip()
+            print(out)
+            assert re.match("%s .+ ok" % pkg_name, out)
+
+    print()
+    assert not os.path.exists(tempdir)
+
+
+def test_install_dependencies_extra_index_url():
+    with tempfile.TemporaryDirectory() as tempdir:
+        mci = MinicondaInstaller(tempdir, verbose=True)
+        mci.download()
+        mci.install_miniconda()
+        mci.update_miniconda_base()
+        mci.install_pip(dependencies=["pypi_pkg_test"], index_url="https://test.pypi.org/simpletest/", extra_index_url="https://test.pypi.org/simple/")
+
+        # Run Miniconda's Python and import the installed dependencies.
+        for pkg_name in ["pypi_pkg_test"]:
+            cmd = [
+                "%s/bin/python" % tempdir,
+                "-c",
+                '''import %s; print("%s %%s ok" %% %s)''' % \
+                (pkg_name, pkg_name, pkg_name)
+            ]
+            out = subprocess.check_output(cmd).decode("utf-8").strip()
+            print(out)
+            assert re.match("%s .+ ok" % pkg_name, out)
+
+    print()
+    assert not os.path.exists(tempdir)
+
+
+def test_install_dependencies_extra_index_urls():
+    with tempfile.TemporaryDirectory() as tempdir:
+        mci = MinicondaInstaller(tempdir, verbose=True)
+        mci.download()
+        mci.install_miniconda()
+        mci.update_miniconda_base()
+        mci.install_pip(dependencies=["pypi_pkg_test"], index_url="https://test.pypi.org/simpletest/", extra_index_url="https://test.pypi.org/simpletest1/, https://test.pypi.org/simple/")
+
+        # Run Miniconda's Python and import the installed dependencies.
+        for pkg_name in ["pypi_pkg_test"]:
+            cmd = [
+                "%s/bin/python" % tempdir,
+                "-c",
+                '''import %s; print("%s %%s ok" %% %s)''' % \
+                (pkg_name, pkg_name, pkg_name)
+            ]
+            out = subprocess.check_output(cmd).decode("utf-8").strip()
+            print(out)
+            assert re.match("%s .+ ok" % pkg_name, out)
+
+    print()
+    assert not os.path.exists(tempdir)
